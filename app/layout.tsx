730952c8d7b5c320054f81f369872285fc876548
KimBergstroem <kimmen_b@hotmail.com>
2024-12-04 18:43:35 +0100

    feat(ui): add responsive footer with social media links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 
+import Footer from "@/components/layout/footer/Footer"
 import Navbar from "@/components/layout/navbar/Navbar"
 
 import "./globals.css"
@@ -16,9 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="sunset">
-      <body>
+      <body className="flex min-h-screen flex-col">
         <Navbar />
-        <main className="container-wrapper py-8">{children}</main>
+        <main className="container-wrapper grow py-8">{children}</main>
+        <Footer />
       </body>
     </html>
   )
