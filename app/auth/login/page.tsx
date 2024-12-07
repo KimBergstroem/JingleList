92281f433b2e964b07f5e2737fcee486d771b87e
KimBergstroem <kimmen_b@hotmail.com>
2024-12-07 14:07:47 +0100

    style: improve auth forms spacing on mobile

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,10 +5,10 @@ import { LoginForm } from "./LoginForm"
 export default function Login() {
   return (
     <>
-      <div className="container-wrapper">
-        <h1 className="mb-4 text-4xl font-bold">Login</h1>
+      <div className="container-wrapper my-8 sm:my-12">
+        <h1 className="mb-6 text-4xl font-bold">Login</h1>
         <LoginForm />
-        <p className="mt-4 text-sm text-gray-500">
+        <p className="mt-6 text-sm text-gray-500">
           Don&apos;t have an account?{" "}
           <Link href="/auth/register">Register</Link>
         </p>
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -5,10 +5,10 @@ import { RegisterForm } from "./RegisterForm"
 export default function Register() {
   return (
     <>
-      <div className="container-wrapper">
-        <h1 className="mb-4 text-4xl font-bold">Register</h1>
+      <div className="container-wrapper my-8 sm:my-12">
+        <h1 className="mb-6 text-4xl font-bold">Register</h1>
         <RegisterForm />
-        <p className="mt-4 text-sm text-gray-500">
+        <p className="mt-6 text-sm text-gray-500">
           Already have an account? <Link href="/auth/login">Login</Link>
         </p>
       </div>
